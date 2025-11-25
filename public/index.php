0857cf51bbe1122bf47df9fc00ad14de5e7f959d
Lara Ferraz <larapfz06@gmail.com>
2025-11-24 22:19:34 -0300

    adicionando shortcut icon

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ session_start(); // ESSENCIAL para acessar $_SESSION
     <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="css/index.css">
-    <link rel="shortcut icon" href="imagens/logoteste.png" >
+    <link rel="shortcut icon" href="images/logomarrom.png" >
 
     <script src="js/bootstrap.bundle.min.js"></script>
 
